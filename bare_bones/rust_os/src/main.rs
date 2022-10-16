a9efb9ced06291c056c9d1d607d754c2ff0dde4e
oopir <gaash.ofir@gmail.com>
2022-10-16 19:39:01 +0300

    started interrupts module

--- bare_bones/rust_os/src/main.rs
+++ bare_bones/rust_os/src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]      // get rid of standard library, which uses os-specific code
 #![no_main]     // get rid of 'c runtime zero', which usually calls main()
+#![feature(abi_x86_interrupt)]  // allow use of needed rust feature that is still 'unstable'
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+pub mod interrupts;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -16,5 +18,15 @@ pub extern "C" fn _start() -> ! {
 
     println!("Hello World{}", "!");
     
+    init();
+
+    x86_64::instructions::interrupts::int3();
+    
+    println!("About to enter infinite loop...");
+
     loop {}
+}
+
+fn init() {
+    interrupts::init_idt();
 }
